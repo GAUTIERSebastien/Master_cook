add63480a4ecc8d8993fc80a1aebeeadede2a357
Sébastien GAUTIER <sgautier034@gmail.com>
2023-07-13 14:33:48 +0200

    Architechture creation

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const { resolve } = require('path');
 const express = require('express');
 const app = express();
-const router = require('./routing');
+const router = require('./routers');
 
 // CONFIGURATION DE L'APP
 app.use( express.static(resolve('public')) );
