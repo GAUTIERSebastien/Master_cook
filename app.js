6d4f3e632caf2217b06e883080638cf3e12094c0
Virginie Giacometti <lana.12@wanadoo.fr>
2023-07-13 16:11:47 +0200

    new Archi

---
+++ app.js
@@ -0,0 +1,10 @@
+const { resolve } = require('path');
+const express = require('express');
+const app = express();
+const router = require('./routing');
+
+// CONFIGURATION DE L'APP
+app.use(express.static(resolve('public')));
+app.use(router);
+
+module.exports = app;
