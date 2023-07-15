3fc47b78d7eb88ebeb5eb95e1bcf9cd51c9baf90
JEREMY BALSEMIN <jeremy.balsemin@wanadoo.fr>
2023-07-15 12:30:10 +0200

    reclone

--- public/js/fetchAllRecipes.js
+++ public/js/fetchAllRecipes.js
@@ -4,59 +4,59 @@ const showElement = document.getElementById('showElement');
 
 
 fetch('https://localhost:4343/recipes', {
-    method: "GET", 
+    method: "GET",
     headers:
     {
         "content-type": "application/json",
 
     },
 }
-) 
-    .then(res=>res.json())   
-    .then(allRecipes =>{
+)
+    .then(res => res.json())
+    .then(allRecipes => {
         // console.log(allRecipes); 
         //console.log(allRecipes.recipes.name);
         allRecipes.forEach(recipe => {
-          // console.log(recipe.recipes);
-          //recuperation des recettes par Pays
-          //console.log(recipe);
-          createMarkup('h1', recipe.name, showElement, );
-          
-        
-           const recettes = recipe.recipes;
-           recettes.forEach(recette =>{
-            // console.log(recette.ingredients);
-            //recuperation de toutes les recettes (uniquement!)
-            //console.log(recette);
-            const titleIng = createMarkup('section', '', showElement, [{class: "recipeName"}])
-
-           createMarkup('h2', recette.title, titleIng );
+            // console.log(recipe.recipes);
+            //recuperation des recettes par Pays
+            //console.log(recipe);
+            createMarkup('h1', recipe.name, showElement,);
 
 
-            const ingredients = recette.ingredients;
-            ingredients.forEach(ingredient =>{  
-               // console.log(ingredient.name);
-               
-                createMarkup('p', ingredient.name, titleIng);
-                //creation des buttons supprimer/modifier (avec des contantes btn edit & delete, le parent sera le titleIng (a recuperer avec la class ou l'id (a voir)))
+            const recettes = recipe.recipes;
+            recettes.forEach(recette => {
+                // console.log(recette.ingredients);
+                //recuperation de toutes les recettes (uniquement!)
+                //console.log(recette);
+                const titleIng = createMarkup('section', '', showElement, [{ class: "recipeName" }])
 
+                createMarkup('h2', recette.title, titleIng);
+
+
+                const ingredients = recette.ingredients;
+                ingredients.forEach(ingredient => {
+                    // console.log(ingredient.name);
+
+                    createMarkup('p', ingredient.name, titleIng);
+                    //creation des buttons supprimer/modifier (avec des contantes btn edit & delete, le parent sera le titleIng (a recuperer avec la class ou l'id (a voir)))
+
+                })
+                const btnEdit = createMarkup('button', 'Modifier', titleIng);
+                btnEdit.addEventListener('click', () => {
+
+                    console.log("Modifier recette :", recette);
+
+                });
+                const btnDelete = createMarkup('button', 'Supprimer', titleIng);
+                btnDelete.addEventListener('click', () => {
+
+                    console.log("supprimer recette ", recette);
+
+                });
             })
-            const btnEdit = createMarkup('button', 'Modifier', titleIng );
-          btnEdit.addEventListener('click', () => {
-            
-            console.log("Modifier recette :", recette);
-
-          });
-          const btnDelete = createMarkup('button', 'Supprimer', titleIng);
-          btnDelete.addEventListener('click', ()=>{
-              
-              console.log("supprimer recette ", recette);
-
-          });
-           })
         })
-            
-        });
 
-        
+    });
+
+
 
--- public/js/script.js
+++ public/js/script.js
@@ -1,11 +1,11 @@
 import { fetchAllRecipes } from './fetchAllRecipes';
 
 /**
- * => si la route /allrecipes et demandée 
-    => alors exécute getallrecipes.js 
+ * => si la route /allrecipes et demandée
+    => alors exécute getallrecipes.js
 
-    * => si la route /....... et demandée 
-    => alors exécute get......js 
+    * => si la route /....... et demandée
+    => alors exécute get......js
 **/
 
 // fetchAllRecipes();
