60b65596d735edff817138e62dc48d12d5033720
Sébastien GAUTIER <sgautier034@gmail.com>
2023-07-15 08:27:41 +0200

    Structuring achitecture and json(add measures)

--- routing/index.js
+++ routing/index.js
@@ -2,10 +2,10 @@
 
 const router = require('express').Router();
 const appRoutes = require('./app.routes');
-// const apiRoutes = require('./api.routes');
+
 
 router.use(appRoutes);
-// router.use(apiRoutes);
+
 
 router.get('*', (req, res) => res.redirect('/home'));
 
