95c1c447ea81b918d90f9c42c24bce05293975cc
Sébastien GAUTIER <sgautier034@gmail.com>
2023-07-18 16:44:34 +0200

    Display form add recipe ok

--- public/js/fetchAllRecipes.js
+++ public/js/fetchAllRecipes.js
@@ -1,16 +1,61 @@
-
 import { createMarkup } from "../utils/createMarkup.js";
-import { fetchDelete } from "../js/fetchDelete.js"
+import { fetchDelete } from "../js/fetchDelete.js";
+import {units} from "../utils/unit.js";
 
 const showElement = document.getElementById('showElement');
 
-// Ajout du bouton "Ajouter une recette"
-const addButton = createMarkup('button', 'Ajouter une recette', showElement, [{ class: "add-recipe btn btn-success" }]);
+
+// Ajout d'un champ ingrédient
+function addIngredientFields(ingredientsDiv, index) {
+    createMarkup('input', '', ingredientsDiv, [{ placeholder: 'Nom de l\'ingredient', name: `ingredientName${index}`, required: 'true' }]);
+    createMarkup('input', '', ingredientsDiv, [{ placeholder: 'Quantité', name: `ingredientQuantity${index}`, required: 'true' }]);
+    
+    const unitSelect = createMarkup('select', '', ingredientsDiv, [{ name: `ingredientUnit${index}`, required: 'true' }]);
+    Object.values(units).forEach(unit => {
+        createMarkup('option', unit, unitSelect);
+    });
+}
+
+//************Button add recipe**********************/
+// Création du conteneur pour le bouton
+const buttonContainer = createMarkup('div', '', document.body, [{ class: "button-container" }]);
+
+// Ajout du bouton "Ajouter une recette" à l'intérieur du conteneur
+const addButton = createMarkup('button', 'Ajouter une recette', buttonContainer, [{ class: "add-recipe btn btn-success" }]);
+
 addButton.addEventListener('click', () => {
-    // Logique pour ajouter une recette ici.
-    console.log("Ajouter recette clicked");
-    location.href = "https://localhost:4343/add-recipe-form"
+    // Création du formulaire d'ajout de recette
+    const form = createMarkup('form', '', document.body, [{ id: "recipe-form" }]);
+    createMarkup('input', '', form, [{ placeholder: 'Titre de la recette', name: 'title', required: 'true' }]);
+    createMarkup('input', '', form, [{ placeholder: 'Pays', name: 'country', required: 'true' }]);
+
+    
+    const ingredientsDiv = createMarkup('div', '', form, [{ id: 'ingredients' }]);
+    createMarkup('h2', 'Ingredients', ingredientsDiv);
+    
+    // Création des champs d'ingrédient par défaut
+    for (let i = 0; i < 3; i++) {
+        addIngredientFields(ingredientsDiv, i);
+    }
+    
+    // Bouton pour ajouter plus d'ingrédients
+    const addIngredientButton = createMarkup('button', 'Ajouter un ingredient', form, [{ type: 'button' }]);
+    let ingredientCount = 3;
+    addIngredientButton.addEventListener('click', () => {
+        addIngredientFields(ingredientsDiv, ingredientCount);
+        ingredientCount++;
+    });
+    
+    // Bouton de soumission du formulaire
+    const submitButton = createMarkup('button', 'Ajouter la recette', form, [{ type: 'submit' }]);
+    form.addEventListener('submit', handleFormSubmit);
+
+    // Insertion du formulaire avant le conteneur du bouton
+    document.body.insertBefore(form, buttonContainer);
 });
+//***************************************************/
+
+
 fetch('https://localhost:4343/recipes', {
     method: "GET",
     headers:
@@ -77,17 +122,11 @@ fetch('https://localhost:4343/recipes', {
                     }
                     else {
                         location.href = "https://localhost:4343/home"
-
                     }
-
-
 
                     location.href = "https://localhost:4343/home"
 
-
                     console.log("supprimer recette ", recette);
-
-
                 });
             })
         })
---
+++ public/utils/unit.js
@@ -0,0 +1,16 @@
+export const units = {
+    "UNIT_GRAM": "g",
+    "UNIT_KILOGRAM": "kg",
+    "UNIT_OBJECT": "pièce",
+    "UNIT_PACK": "sachet",
+    "UNIT_SLICE": "tranche",
+    "UNIT_MILLILITERS": "ml",
+    "UNIT_LITER": "l",
+    "UNIT_TABLESPOON": "c.à.s",
+    "UNIT_TEASPOON": "c.à.c",
+    "UNIT_CUBE": "cube",
+    "UNIT_POD": "gousse",
+    "UNIT_PINCH": "pincée",
+    "UNIT_SHEET": "feuille",
+    "UNIT_PM": "selon les goûts"
+};
