714af3bd08590f5bfe168bc71dd5075eb4046930
JEREMY BALSEMIN <jeremy.balsemin@wanadoo.fr>
2023-07-18 12:24:57 +0200

    update of fetch all recipes for fonction

--- public/js/fetchAllRecipes.js
+++ public/js/fetchAllRecipes.js
@@ -1,5 +1,5 @@
 import { createMarkup } from "../untils/createMarkup.js";
-import {fetchDelete} from "../js/fetchDelete.js"
+import { fetchDelete } from "../js/fetchDelete.js"
 
 const showElement = document.getElementById('showElement');
 
@@ -12,10 +12,7 @@ fetch('https://localhost:4343/recipes', {
 
     },
 }
-
-) 
- 
-   
+)
     .then(res => res.json())
     .then(allRecipes => {
         // console.log(allRecipes); 
@@ -25,17 +22,18 @@ fetch('https://localhost:4343/recipes', {
             //recuperation des recettes par Pays
             //console.log(recipe);
             createMarkup('h1', recipe.name, showElement,);
-            const pays = createMarkup('section', "", showElement, [{ class: "row" }]);
 
+            const country = createMarkup('section', "", showElement, [{ class: "row" }]);
 
             const recettes = recipe.recipes;
             recettes.forEach(recette => {
                 // console.log(recette.ingredients);
                 //recuperation de toutes les recettes (uniquement!)
                 //console.log(recette);
-                const titleIng = createMarkup('article', '', pays, [{ class: "recipeName card col-4" }])
+                const titleIng = createMarkup('article', '', country, [{ class: "card col-4 recipeName" }])
 
                 createMarkup('h2', recette.title, titleIng, [{ class: "card-title" }]);
+
 
                 const cardBody = createMarkup('div', "", titleIng, [{ class: "card-body" }]);
 
@@ -43,54 +41,50 @@ fetch('https://localhost:4343/recipes', {
                 ingredients.forEach(ingredient => {
                     // console.log(ingredient.name);
 
+
                     createMarkup('h3', ingredient.name, cardBody);
                     createMarkup('span', ingredient.quantity, cardBody);
                     createMarkup('span', ingredient.unit, cardBody); //à modifier pour avoir les resultat du 2eme tableau
 
 
-
-                    //creation des buttons supprimer/modifier (avec des contantes btn edit & delete, le parent sera le titleIng (a recuperer avec la class ou l'id (a voir)))
-
                 })
                 const buttonDiv = createMarkup('div', '', cardBody)
+
                 const btnEdit = createMarkup('button', 'Modifier', buttonDiv, [
                     { class: 'edit-recipe btn btn-warning' },
                     { 'data-id': recette.id },
                     { 'data-ingredients': JSON.stringify(recette.ingredients) }
                 ]);
-                btnEdit.addEventListener("click", () => {
+                btnEdit.addEventListener('click', () => {
                     handleEditButtonClick(recette.id, JSON.stringify(recette.ingredients));
+                    console.log("Modifier recette :", recette);
+
                 });
+                const btnDelete = createMarkup('button', 'Supprimer', buttonDiv, [{ class: "reload btn btn-danger" }]);
+                btnDelete.addEventListener('click', () => {
+                    console.log(recette.id);
+                    if (confirm('Souhaitez-vous confirmer ?')) {
+
+                        fetchDelete(recette.id)
+                        location.href = "https://localhost:4343/home"
+                    }
+                    else {
+                        location.href = "https://localhost:4343/home"
+
+                    }
 
 
-      const btnDelete = createMarkup('button', 'Supprimer', buttonDiv, [{class:'btn btn-danger'}]);
-      btnDelete.addEventListener('click', ()=>{
-            console.log(recette.id);
-            if(confirm('Souhaitez-vous confirmer ?')) {
-              
-              fetchDelete(recette.id)
-              location.href="https://localhost:4343/home"
-            }
-            else {
-              location.href="https://localhost:4343/home"
-              
-            }
+
+                    location.href = "https://localhost:4343/home"
 
 
-            
-             location.href="https://localhost:4343/home"
-              
-
-            console.log("supprimer recette ", recette);
-            
-
-          });
-           })
+                    console.log("supprimer recette ", recette);
 
 
+                });
+            })
         })
 
     });
-
 
 
