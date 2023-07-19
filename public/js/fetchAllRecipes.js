c9783e01e13935078fe34a85444cc4b3ecc14c61
CherHamza <hamzacheracher1@hotmail.fr>
2023-07-19 22:07:08 +0200

    filter recipes done (title, name of the recipe + ingredients and units)

--- public/js/fetchAllRecipes.js
+++ public/js/fetchAllRecipes.js
@@ -1,104 +1,228 @@
-import { createMarkup } from "../untils/createMarkup.js";
-import { fetchDelete } from "../js/fetchDelete.js"
+// import { createMarkup } from "../untils/createMarkup.js";
+// import { fetchDelete } from "../js/fetchDelete.js"
 
 
-const units = {
-    "UNIT_GRAM": "gram",
-    "UNIT_KILOGRAM": "kilogram",
-    "UNIT_OBJECT": "object",
-    "UNIT_PACK": "bag",
-    "UNIT_SLICE": "slice",
-    "UNIT_MILLILITERS": "millilitre",
-    "UNIT_LITER": "litre",
-    "UNIT_TABLESPOON": "tablespoon",
-    "UNIT_TEASPOON": "teaspoon",
-    "UNIT_CUBE": "cube",
-    "UNIT_POD": "pod",
-    "UNIT_PINCH": "pinch",
-    "UNIT_SHEET": "sheet",
-    "UNIT_PM": "to taste"
-  };
+// const units = {
+//     "UNIT_GRAM": "gram",
+//     "UNIT_KILOGRAM": "kilogram",
+//     "UNIT_OBJECT": "object",
+//     "UNIT_PACK": "bag",
+//     "UNIT_SLICE": "slice",
+//     "UNIT_MILLILITERS": "millilitre",
+//     "UNIT_LITER": "litre",
+//     "UNIT_TABLESPOON": "tablespoon",
+//     "UNIT_TEASPOON": "teaspoon",
+//     "UNIT_CUBE": "cube",
+//     "UNIT_POD": "pod",
+//     "UNIT_PINCH": "pinch",
+//     "UNIT_SHEET": "sheet",
+//     "UNIT_PM": "to taste"
+//   };
   
+// const showElement = document.getElementById('showElement');
+
+// fetch('https://localhost:4343/recipes', {
+//     method: "GET",
+//     headers:
+//     {
+//         "content-type": "application/json",
+
+//     },
+// }
+// )
+//     .then(res => res.json())
+//     .then(allRecipes => {
+//         console.log(allRecipes); 
+//         //console.log(allRecipes.recipes.name);
+//         allRecipes.forEach(recipe => {
+//             // console.log(recipe.recipes);
+//             //recuperation des recettes par Pays
+//             //console.log(recipe);
+//             createMarkup('h1', recipe.name, showElement,);
+
+//             const country = createMarkup('section', "", showElement, [{ class: "row" }]);
+
+//             const recettes = recipe.recipes;
+//             recettes.forEach(recette => {
+//                 // console.log(recette.ingredients);
+//                 //recuperation de toutes les recettes (uniquement!)
+//                 //console.log(recette);
+//                 const titleIng = createMarkup('article', '', country, [{ class: "card col-4 recipeName" }])
+
+//                 createMarkup('h2', recette.title, titleIng, [{ class: "card-title" }]);
+
+
+//                 const cardBody = createMarkup('div', "", titleIng, [{ class: "card-body" }]);
+
+//                 const ingredients = recette.ingredients;
+//                 ingredients.forEach(ingredient => {
+                   
+//                     createMarkup('h3', ingredient.name, cardBody);
+//                     createMarkup('span', ingredient.quantity, cardBody);
+//                     createMarkup('span', units[ingredient.unit], cardBody);
+//                   });
+                  
+//                 const buttonDiv = createMarkup('div', '', cardBody)
+
+//                 const btnEdit = createMarkup('button', 'Modifier', buttonDiv, [
+//                     { class: 'edit-recipe btn btn-warning' },
+//                     { 'data-id': recette.id },
+//                     { 'data-ingredients': JSON.stringify(recette.ingredients) }
+//                 ]);
+//                 btnEdit.addEventListener('click', () => {
+//                     handleEditButtonClick(recette.id, JSON.stringify(recette.ingredients));
+//                     console.log("Modifier recette :", recette);
+
+//                 });
+//                 const btnDelete = createMarkup('button', 'Supprimer', buttonDiv, [{ class: "reload btn btn-danger" }]);
+//                 btnDelete.addEventListener('click', () => {
+//                     console.log(recette.id);
+//                     if (confirm('Souhaitez-vous confirmer ?')) {
+
+//                         fetchDelete(recette.id)
+//                         location.href = "https://localhost:4343/home"
+//                     }
+//                     else {
+//                         location.href = "https://localhost:4343/home"
+
+//                     }
+
+
+
+//                     location.href = "https://localhost:4343/home"
+
+
+//                     console.log("supprimer recette ", recette);
+
+
+//                 });
+//             });
+            
+//         })
+        
+
+//     });
+
+import { createMarkup } from "../untils/createMarkup.js";
+import { fetchDelete } from "../js/fetchDelete.js";
+
+import {units} from "../untils/units.js"
+ import {createFilteredRecipeElements} from  "../js/filterRecipe.js"
+import {filterRecipes} from  "../js/filterRecipe.js"
+
+
+
 const showElement = document.getElementById('showElement');
 
 fetch('https://localhost:4343/recipes', {
-    method: "GET",
-    headers:
-    {
-        "content-type": "application/json",
+  method: "GET",
+  headers: {
+    "content-type": "application/json",
+  },
+})
 
-    },
-}
-)
-    .then(res => res.json())
-    .then(allRecipes => {
-        console.log(allRecipes); 
-        //console.log(allRecipes.recipes.name);
-        allRecipes.forEach(recipe => {
-            // console.log(recipe.recipes);
-            //recuperation des recettes par Pays
-            //console.log(recipe);
-            createMarkup('h1', recipe.name, showElement,);
+  .then(res => res.json())
+  .then(allRecipes => {
+    console.log(allRecipes);
+    
 
-            const country = createMarkup('section', "", showElement, [{ class: "row" }]);
+    allRecipes.forEach(recipeGroup => {
+      createMarkup('h1', recipeGroup.name, showElement);
 
-            const recettes = recipe.recipes;
-            recettes.forEach(recette => {
-                // console.log(recette.ingredients);
-                //recuperation de toutes les recettes (uniquement!)
-                //console.log(recette);
-                const titleIng = createMarkup('article', '', country, [{ class: "card col-4 recipeName" }])
+      const country = createMarkup('section', "", showElement, [{ class: "row" }]);
 
-                createMarkup('h2', recette.title, titleIng, [{ class: "card-title" }]);
+      const recipes = recipeGroup.recipes;
+      recipes.forEach(recipe => {
+        const titleIng = createMarkup('article', '', country, [{ class: "card col-4 recipeName" }]);
 
+        createMarkup('h2', recipe.title, titleIng, [{ class: "card-title" }]);
 
-                const cardBody = createMarkup('div', "", titleIng, [{ class: "card-body" }]);
+        const cardBody = createMarkup('div', "", titleIng, [{ class: "card-body" }]);
 
-                const ingredients = recette.ingredients;
-                ingredients.forEach(ingredient => {
-                   
-                    createMarkup('h3', ingredient.name, cardBody);
-                    createMarkup('span', ingredient.quantity, cardBody);
-                    createMarkup('span', units[ingredient.unit], cardBody);
-                  });
-                  
-                const buttonDiv = createMarkup('div', '', cardBody)
+        const ingredients = recipe.ingredients;
+        ingredients.forEach(ingredient => {
+            
+          createMarkup('h3', ingredient.name, cardBody);
+          
+          createMarkup('span', ingredient.quantity, cardBody);
+        
+          createMarkup('span', units[ingredient.unit], cardBody);
+        });
 
-                const btnEdit = createMarkup('button', 'Modifier', buttonDiv, [
-                    { class: 'edit-recipe btn btn-warning' },
-                    { 'data-id': recette.id },
-                    { 'data-ingredients': JSON.stringify(recette.ingredients) }
-                ]);
-                btnEdit.addEventListener('click', () => {
-                    handleEditButtonClick(recette.id, JSON.stringify(recette.ingredients));
-                    console.log("Modifier recette :", recette);
+        const buttonDiv = createMarkup('div', '', cardBody);
 
-                });
-                const btnDelete = createMarkup('button', 'Supprimer', buttonDiv, [{ class: "reload btn btn-danger" }]);
-                btnDelete.addEventListener('click', () => {
-                    console.log(recette.id);
-                    if (confirm('Souhaitez-vous confirmer ?')) {
+        const btnEdit = createMarkup('button', 'Modifier', buttonDiv, [
+          { class: 'edit-recipe btn btn-warning' },
+          { 'data-id': recipe.id },
+          { 'data-ingredients': JSON.stringify(recipe.ingredients) }
+        ]);
+        btnEdit.addEventListener('click', () => {
+          handleEditButtonClick(recipe.id, JSON.stringify(recipe.ingredients));
+          console.log("Modifier recette :", recipe);
+        });
 
-                        fetchDelete(recette.id)
-                        location.href = "https://localhost:4343/home"
-                    }
-                    else {
-                        location.href = "https://localhost:4343/home"
-
-                    }
-
-
-
-                    location.href = "https://localhost:4343/home"
-
-
-                    console.log("supprimer recette ", recette);
-
-
-                });
-            })
-        })
-
+        const btnDelete = createMarkup('button', 'Supprimer', buttonDiv, [{ class: "reload btn btn-danger" }]);
+        btnDelete.addEventListener('click', () => {
+          console.log(recipe.id);
+          if (confirm('Souhaitez-vous confirmer ?')) {
+            fetchDelete(recipe.id);
+            location.href = "https://localhost:4343/home";
+          } else {
+            location.href = "https://localhost:4343/home";
+          }
+          console.log("supprimer recette ", recipe);
+        });
+      });
     });
+    // Gestionnaire d'événements pour le formulaire de filtrage
+    document.getElementById('filterForm').addEventListener('submit', event => {
+        event.preventDefault();
+       
+    
+        const recipeNameInput = document.getElementById('recipeNameInput');
+        const recipeName = recipeNameInput.value.toLowerCase();
 
+        
+        const resultFilter= filterRecipes(allRecipes, recipeName)
+   
+        createFilteredRecipeElements(resultFilter) 
 
+    
+   
+    })
+
+    // Ajoute cette fonction pour extraire tous les ingrédients uniques de toutes les recettes
+function extractUniqueIngredients(allRecipes) {
+    const uniqueIngredients = new Set();
+  
+    allRecipes.forEach(recipeGroup => {
+      recipeGroup.recipes.forEach(recipe => {
+        recipe.ingredients.forEach(ingredient => {
+          uniqueIngredients.add(ingredient.name.toLowerCase());
+        });
+      });
+    });
+  
+    return Array.from(uniqueIngredients);
+  }
+  
+  // Ajoute cette fonction pour mettre à jour dynamiquement les options du champ de sélection avec les ingrédients
+  function updateIngredientFilterOptions(allRecipes) {
+    const ingredientFilter = document.getElementById('ingredientFilter');
+    const uniqueIngredients = extractUniqueIngredients(allRecipes);
+  
+    ingredientFilter.innerHTML = '<option value="">Tous les ingrédients</option>';
+    uniqueIngredients.forEach(ingredient => {
+      const option = document.createElement('option');
+      option.value = ingredient;
+      option.textContent = ingredient;
+      ingredientFilter.appendChild(option);
+    });
+  }
+  
+  // Au début du bloc .then() dans fetchAllRecipes.js, appelle la fonction pour mettre à jour les options du champ de sélection
+ 
+    // ...
+  
+
+})   
---
+++ public/js/filterRecipe.js
@@ -0,0 +1,60 @@
+
+
+    import { createMarkup } from "../untils/createMarkup.js";
+    import {units} from "../untils/units.js"
+    // Fonction pour filtrer les recettes par nom
+
+    export function filterRecipes(allRecipes, recipeName) {
+      const filteredRecipes = [];
+  
+      allRecipes.forEach(recipeGroup => {
+        const filteredGroup = {
+          name: recipeGroup.name,
+          recipes: []
+        };
+  
+        recipeGroup.recipes.forEach(recipe => {
+          // Vérifie si le titre de la recette correspond à la recherche (ignorer la casse)
+          if (recipe.title.toLowerCase().includes(recipeName)) {
+            filteredGroup.recipes.push(recipe);
+            
+          }
+        });
+  
+        if (filteredGroup.recipes.length > 0) {
+          filteredRecipes.push(filteredGroup);
+        }
+      });
+  
+      return filteredRecipes;
+    }
+  
+    // Fonction pour générer les éléments HTML de la recette filtrée
+    export function createFilteredRecipeElements(filteredRecipes) {
+      showElement.innerHTML = '';
+  
+      filteredRecipes.forEach(recipeGroup => {
+        createMarkup('h2', recipeGroup.name, showElement);
+  
+        const groupElement = createMarkup('div', '', showElement);
+        const recipes = recipeGroup.recipes;
+  
+        recipes.forEach(recipe => {
+          const recipeElement = createMarkup('div', '', groupElement, [{ class: "recipe" }]);
+          createMarkup('h3', recipe.title, recipeElement);
+  
+          const ulElement = createMarkup('ul', '', recipeElement);
+          recipe.ingredients.forEach(ingredient => {
+            if (ingredient.unit === "UNIT_PM") {
+              createMarkup('li', `${ingredient.name} - ${units[ingredient.unit]}`, ulElement);
+              
+            } else {
+            
+              createMarkup('li', `${ingredient.name} - ${ingredient.quantity} ${units[ingredient.unit]}`, ulElement);
+            }
+            
+          });
+        });
+      });
+    }
+
